2908518e7c0db12b02c277afd4a8c0ba67623bb1
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-11 19:15:26 +0100

    feat(server): set up MongoDB connection and update dependencies
    used .env for hiding the URI from the database connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+app.use(express.json());
+const uri = process.env.URI;
+const client = new MongoClient(uri);
+let db;
 
-app.get("/", (req, res) => {
-	res.send({ message: "hellooooooooo" });
-});
-app.listen(3000, (err) => {
-	if (!err) {
-		console.log("running on port" + 3000);
-	} else {
-		console.error(err);
+async function connectDB() {
+	try {
+		await client.connect();
+		db = client.db("PokemonUsers");
+		console.log("Connected to MongoDB");
+		app.listen(3000, () => console.log("Server running on port 3000"));
+	} catch (err) {
+		console.error("Error connecting to MongoDB:", err);
 	}
-});
+}
+connectDB();
