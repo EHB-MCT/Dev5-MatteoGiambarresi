1a7dfaf58aa54925d6a2fb7dc1e157219ec216d0
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-11 17:13:28 +0100

    feat (server) : set up Express server with nodemon for development
    - Updated package.json to use nodemon for the start script and added it as a devDependency.
    - Created a .dockerignore file to exclude node_modules from Docker builds.
    - Added initial index.js file with a basic Express server responding with a message.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send({ message: "hello" });
+	res.send({ message: "hellooooooooo" });
 });
 app.listen(3000, (err) => {
 	if (!err) {
