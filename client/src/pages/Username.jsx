d01cab9c61d1e2045f617b1888455d2db77dedf9
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-18 20:01:38 +0100

    feat(client): Updated Username component to handle user input and connected it to the back-end.

--- client/src/pages/Username.jsx
+++ client/src/pages/Username.jsx
@@ -1,14 +1,37 @@
 import { Link } from "react-router";
+import { useState } from "react";
+function Username() {
+	const [name, setName] = useState("");
+	// console.log(name);
+	async function getPostData(url, method, data) {
+		let resp = await fetch(url, {
+			method: method,
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+		return await resp.json();
+	}
+	function handleSubmit() {
+		getPostData("http://localhost:3000/registerName", "POST", { name }).then((data) => {
+			console.log(data);
+			alert(data.message);
+		});
+	}
 
-function UserName() {
 	return (
 		<div>
-			<h1>UserNamePage</h1>
-			<Link to="/teamSelector">
+			<h1>Enter your username</h1>
+			<input type="text" onChange={(event) => setName(event.target.value)} placeholder="name" />
+			<button onClick={handleSubmit} type="submit">
+				Submit
+			</button>
+			{/* <Link to="/teamSelector">
 				<button>start experience</button>
-			</Link>
+			</Link> */}
 		</div>
 	);
 }
 
-export default UserName;
+export default Username;
