4c6e53132a87241f047e8517717724589213e34a
MatteoGiambarresi <220775874+Matgiam@users.noreply.github.com>
2025-11-19 16:59:41 +0100

    feat(client): added hardcoded pokemon list to TeamSelector page and display it

--- client/src/pages/TeamSelector.jsx
+++ client/src/pages/TeamSelector.jsx
@@ -1,6 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 function TeamSelector() {
 	const [username, setUsername] = useState("");
+	const [pokemonList, setPokemonList] = useState([]);
+	const pokemons = [
+		"pikachu",
+		"bulbasaur",
+		"charmander",
+		"squirtle",
+		"pidgey",
+		"rattata",
+		"jigglypuff",
+		"meowth",
+		"psyduck",
+		"machop",
+		"geodude",
+		"eevee",
+	];
 	useEffect(() => {
 		const userString = localStorage.getItem("user");
 		const userParse = JSON.parse(userString);
@@ -8,10 +23,17 @@ function TeamSelector() {
 		setUsername(userParse.name);
 	}, []);
 
+	useEffect(() => {
+		setPokemonList(pokemons);
+	}, []);
+
 	return (
 		<div>
 			<h1>TeamSelector Page</h1>
 			<h2>{username}</h2>
+			{pokemonList.map((pokemon) => (
+				<p key={pokemon}>{pokemon}</p>
+			))}
 		</div>
 	);
 }
